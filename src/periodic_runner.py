6623baab323984782b004395c672786e46a00525
zephyrus8888 <piotr.fryc@gmail.com>
2020-05-28 11:15:06 +0200

    scaffold

--- src/periodic_runner.py
+++ src/periodic_runner.py
@@ -3,13 +3,9 @@ from time import sleep
 
 class PeriodicRunner:
 
-    def __init__(self, period=1.0, callback=None, **kwargs):
+    def __init__(self, period=1.0):
         self.period = period
-        self.callback = callback
-        self.kwargs = kwargs
 
     def start(self):
         while True:
             sleep(self.period)
-            if self.callback:
-                self.callback(**self.kwargs)
--- src/user_code.py
+++ src/user_code.py
@@ -4,11 +4,5 @@ from periodic_runner import PeriodicRunner
 """Your definitions here"""
 
 
-def my_callback(name=None):
-    print("Hello", name)
-
-
 if __name__ == "__main__":
     """Your code here"""
-    x = PeriodicRunner(callback=my_callback, name="Peter")
-    x.start()
